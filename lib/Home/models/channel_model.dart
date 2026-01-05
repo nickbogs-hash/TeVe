02b8caa9d62319b410f6fac78edba4bb4df92d1e
nickbogs-hash <nickbogs@gmail.com>
2026-01-05 17:52:46 +1100

    Update channel_model.dart

--- lib/Home/models/channel_model.dart
+++ lib/Home/models/channel_model.dart
@@ -1,56 +1,25 @@
-import 'package:teve/Home/models/categories_model.dart';
-import 'package:teve/Home/models/countries_model.dart';
-import 'package:teve/Home/models/languages_model.dart';
-import 'package:teve/Home/models/tvg_model.dart';
-
 class ChannelModel {
   String? name;
   String? logo;
   String? url;
-  List<Categories>? categories;
-  List<Countries>? countries;
-  List<Languages>? languages;
-  Tvg? tvg;
+  String? category; // We will fake this if missing
 
-  ChannelModel(
-      {this.name,
-      this.logo,
-      this.url,
-      this.categories,
-      this.countries,
-      this.languages,
-      this.tvg});
+  ChannelModel({this.name, this.logo, this.url, this.category});
 
-  ChannelModel.fromJson(Map<String, dynamic> json) {
-    name = json['name'];
-    logo = json['logo'];
-    url = json['url'];
-    if (json['categories'] != null) {
-      categories = <Categories>[];
-      json['categories'].forEach((v) {
-        categories!.add(Categories.fromJson(v));
-      });
-    }
-    if (json['countries'] != null) {
-      countries = <Countries>[];
-      json['countries'].forEach((v) {
-        countries!.add(Countries.fromJson(v));
-      });
-    }
-    if (json['languages'] != null) {
-      languages = <Languages>[];
-      json['languages'].forEach((v) {
-        languages!.add(Languages.fromJson(v));
-      });
-    }
-    tvg = json['tvg'] != null ? Tvg.fromJson(json['tvg']) : null;
+  // This is the translator. It takes the new JSON and fits it into the app's boxes.
+  factory ChannelModel.fromJson(Map<String, dynamic> json) {
+    return ChannelModel(
+      name: json['name'] ?? "Unknown Channel",
+      logo: json['logo'] ?? "",
+      url: json['url'] ?? "",
+      // The new list might not have categories, so we give it a default one
+      category: json['category'] ?? "General", 
+    );
   }
+}
 
-  Map<String, dynamic> toJson() {
-    return {
-      "stream_link": url,
-      "category": categories![0].name,
-      "channel_name": name
-    };
-  }
+// We also need to fake the Category structure so the Home Screen doesn't crash
+class CategoryModel {
+  String? name;
+  CategoryModel({this.name});
 }
